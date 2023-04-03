b1d9333bc8ac52048fc9d95ef2c0e3a50c85d0d6
MdAshrafUllah <mdashrafullah47@gmail.com>
2023-04-03 14:08:02 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 import 'Services/AuthenticationService.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
@@ -23,7 +23,6 @@ class loginPage extends StatefulWidget {
 }
 
 class _loginPageState extends State<loginPage> {
-
   final AuthenticationServices _auth = AuthenticationServices();
 
   TextEditingController _emailController = TextEditingController();
@@ -39,26 +38,38 @@ class _loginPageState extends State<loginPage> {
           children: [
             Container(
               margin: EdgeInsets.only(top: 40),
-              child: Image.asset('assets/image/app_logo.png',height: 72,width: 72,),
+              child: Image.asset(
+                'assets/image/app_logo.png',
+                height: 72,
+                width: 72,
+              ),
             ),
             Container(
               margin: EdgeInsets.only(top: 60),
-              child: Text('Login',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.black54,),),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54,
+                ),
+              ),
               alignment: Alignment.topLeft,
               padding: EdgeInsets.only(left: 20),
             ),
             Container(
-              margin: EdgeInsets.only(left: 20, right: 20,top: 20),
-              padding: EdgeInsets.only(left: 20,right: 20),
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
               height: 70,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
                 color: Colors.grey[200],
-                boxShadow: [BoxShadow(
-                    offset: Offset(0,10),
-                    blurRadius: 50,
-                    color: Color(0xFFEEEEEE)
-                )],
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: Color(0xFFEEEEEE))
+                ],
               ),
               alignment: Alignment.center,
               child: TextField(
@@ -88,17 +99,18 @@ class _loginPageState extends State<loginPage> {
               ),
             ),
             Container(
-              margin: EdgeInsets.only(left: 20, right: 20,top: 20),
-              padding: EdgeInsets.only(left: 20,right: 20),
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
               height: 60,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
                 color: Colors.grey[200],
-                boxShadow: [BoxShadow(
-                    offset: Offset(0,10),
-                    blurRadius: 50,
-                    color: Color(0xFFEEEEEE)
-                )],
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: Color(0xFFEEEEEE))
+                ],
               ),
               alignment: Alignment.center,
               child: TextField(
@@ -118,44 +130,56 @@ class _loginPageState extends State<loginPage> {
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
-
                   enabledBorder: InputBorder.none,
                   focusedBorder: InputBorder.none,
                   suffixIcon: IconButton(
                     icon: Icon(
                       _passwordVisible
-                          ? Icons.visibility_off : Icons.visibility, color: Colors.black54,
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                      color: Colors.black54,
                     ),
-                    onPressed: (){
+                    onPressed: () {
                       setState(() {
                         _passwordVisible = !_passwordVisible;
                       });
                     },
                   ),
                 ),
-                  // onChanged: (value){
-                  //   setState(() {
-                  //     password = value;
-                  //   });
-                  // }
+                // onChanged: (value){
+                //   setState(() {
+                //     password = value;
+                //   });
+                // }
               ),
             ),
             Container(
-              padding: EdgeInsets.only(left: 20,top: 30),
+              padding: EdgeInsets.only(left: 20, top: 30),
               alignment: Alignment.topLeft,
-              child: Text('Forget your password?',style: TextStyle(color: Color(0xFFEA3B4A),fontSize: 18,fontWeight: FontWeight.w500),),
+              child: Text(
+                'Forget your password?',
+                style: TextStyle(
+                    color: Color(0xFFEA3B4A),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500),
+              ),
             ),
             Container(
               child: InkWell(
                 child: Container(
                   height: 100,
                   width: double.infinity,
-                  decoration: BoxDecoration(color: Color(0xFFEA3B4A),borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20),),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEA3B4A),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
                     boxShadow: [
                       BoxShadow(
                         color: Colors.grey.withOpacity(0.5),
                         blurRadius: 7,
-                        offset: Offset(0,5),
+                        offset: Offset(0, 5),
                       ),
                     ],
                   ),
@@ -165,17 +189,30 @@ class _loginPageState extends State<loginPage> {
                         Container(
                           height: 20,
                           width: double.infinity,
-                          decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20),),),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
+                          ),
                         ),
                         Container(
                           margin: EdgeInsets.only(top: 26),
-                          child: Text('Login',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white,),),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
                 ),
-                onTap: (){
+                onTap: () {
                   signUser();
                 },
               ),
@@ -183,28 +220,43 @@ class _loginPageState extends State<loginPage> {
             Container(
               margin: EdgeInsets.only(top: 20),
               child: RichText(
-                text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "Don't have an account?",
-                        style: TextStyle(color: Colors.black54,fontSize: 18,fontWeight: FontWeight.w500,),
-                      ),
-                      TextSpan(
-                        text: " Sign Up",
-                        style: TextStyle(color: Color(0xFFEA3B4A),fontSize: 18,fontWeight: FontWeight.w500,),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = (){
-                            Navigator.push(context,
-                                MaterialPageRoute(builder: (Context) => signupPage()));
-                          },
-                      ),
-                    ]
-                ),
+                text: TextSpan(children: <TextSpan>[
+                  TextSpan(
+                    text: "Don't have an account?",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextSpan(
+                    text: " Sign Up",
+                    style: TextStyle(
+                      color: Color(0xFFEA3B4A),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (Context) => signupPage()));
+                      },
+                  ),
+                ]),
               ),
             ),
             Container(
               margin: EdgeInsets.only(top: 50),
-              child: Text('Or sign in with:',style: TextStyle(color: Colors.black54,fontSize: 14,fontWeight: FontWeight.w500,),),
+              child: Text(
+                'Or sign in with:',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
             Container(
               margin: EdgeInsets.only(top: 10),
@@ -216,16 +268,31 @@ class _loginPageState extends State<loginPage> {
                     padding: EdgeInsets.all(12),
                     height: 50,
                     width: 120,
-                    decoration: BoxDecoration(color: Color(0xFFF7F7F7),borderRadius: BorderRadius.circular(15)),
-                    child: Image.asset('assets/image/fb_logo.png',height: 0.1,width: 0.1,),
+                    decoration: BoxDecoration(
+                        color: Color(0xFFF7F7F7),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Image.asset(
+                      'assets/image/fb_logo.png',
+                      height: 0.1,
+                      width: 0.1,
+                    ),
                   ),
-                  SizedBox(width: 50,),
+                  SizedBox(
+                    width: 50,
+                  ),
                   Container(
                     padding: EdgeInsets.all(12),
                     height: 50,
                     width: 120,
-                    decoration: BoxDecoration(color: Color(0xFFF7F7F7),borderRadius: BorderRadius.circular(15),),
-                    child: Image.asset('assets/image/google_logo.png',height: 0.1,width: 0.1,),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF7F7F7),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Image.asset(
+                      'assets/image/google_logo.png',
+                      height: 0.1,
+                      width: 0.1,
+                    ),
                   ),
                 ],
               ),
@@ -236,14 +303,16 @@ class _loginPageState extends State<loginPage> {
     );
   }
 
-  void signUser() async{
-    dynamic authResult = await _auth.loginUser(_emailController.text, _passwordController.text);
-    if(authResult == null){
+  void signUser() async {
+    dynamic authResult =
+        await _auth.loginUser(_emailController.text, _passwordController.text);
+    if (authResult == null) {
       print('Sign in error. Could Not be able to login');
-    }else{
+    } else {
       _emailController.clear();
       _passwordController.clear();
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (Context) => homePage()));
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (Context) => homePage()));
     }
   }
 }
